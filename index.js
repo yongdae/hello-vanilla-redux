43cf93995d51ee5603dc50776ff585705fa38437
yongdae <7942yongdae@gmail.com>
2020-09-28 20:18:45 +0900

    feat: Add reducer

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ const initialState = {
   toggle: false,
   counter: 0,
 };
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+  }
+}
